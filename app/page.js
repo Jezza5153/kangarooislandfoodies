e8e62284d0653d71cfb0d9b59e83632786119285
Jeremy Arrascaeta <jeremyarrascaeta@Jeremys-MacBook-Air.local>
2025-04-25 18:04:19 +0930

    Use app/page.js for MapView homepage

--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
+'use client';
 import dynamic from 'next/dynamic';
 
 const MapView = dynamic(() => import('../components/MapView'), { ssr: false });
 
 export default function Home() {
   return (
-    <main>
+    <main style={{ padding: '0 1rem' }}>
       <h1>Kangaroo Island Foodies</h1>
       <MapView />
-      {/* TODO: below here we’ll add listings, filters, etc. */}
     </main>
   );
 }
